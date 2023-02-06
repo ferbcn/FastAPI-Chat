66adfe12a90c170ebd845d75d64e753340bb3483
garcifer <garcifer@students.zhaw.ch>
2023-02-06 15:30:10 +0100

    app

---
+++ app/main.py
@@ -0,0 +1,38 @@
+from fastapi import FastAPI, WebSocket
+import random
+from fastapi.responses import PlainTextResponse
+
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+
+
+# Create application
+app = FastAPI(title='FastAPI Templates')
+
+app.mount("/static", StaticFiles(directory="app/static"), name="static")
+templates = Jinja2Templates(directory="app/templates")
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/items/{id}", response_class=HTMLResponse)
+async def read_item(request: Request, id: str):
+    return templates.TemplateResponse("item.html", {"request": request, "id": id})
+
+
+@app.get("/socket", response_class=HTMLResponse)
+async def index(request: Request):
+    return templates.TemplateResponse("socket.html", {"request": request})
+
+
+@app.websocket("/ws")
+async def websocket_endpoint(websocket: WebSocket):
+   await websocket.accept()
+   while True:
+      data = await websocket.receive_text()
+      await websocket.send_text(f"Message text was: {data}")
