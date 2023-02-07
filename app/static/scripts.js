c64a5921e1a3e37ed35274165b684a16546045ca
garcifer <garcifer@students.zhaw.ch>
2023-02-07 08:58:20 +0100

    localhost

--- app/static/scripts.js
+++ app/static/scripts.js
@@ -1,4 +1,4 @@
-var ws = new WebSocket("ws://localhost:8000/ws");
+var ws = new WebSocket("ws://art-intel.site:8000/ws");
 
 ws.onmessage = function(event) {
    var messages = document.getElementById('messages')
