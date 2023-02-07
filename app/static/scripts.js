2c1a99ef066ff8de70eafc4fd1c67028f4bfdb3e
garcifer <garcifer@students.zhaw.ch>
2023-02-07 13:22:55 +0100

    rtt added

--- app/static/scripts.js
+++ app/static/scripts.js
@@ -3,22 +3,22 @@ var ws = new WebSocket("wss://art-intel.site/ws");
 
 var startTime;
 
-
 ws.onmessage = function(event) {
     var endTime = new Date();
     var rtt = endTime - startTime;
     var messages = document.getElementById('messages')
     var message = document.createElement('li')
     var text = event.data;
-    if (isNan(rtt)){
-    text += " (RTT: " + rtt + "ms)";
-    var content = document.createTextNode(text)
-    message.appendChild(content)
-    messages.appendChild(message)
-};
+    if (!isNan(rtt)){
+        text += " (RTT: " + rtt + "ms)";
+    }
+    var content = document.createTextNode(text);
+    message.appendChild(content);
+    messages.appendChild(message);
+}
 
 function sendMessage(event) {
-    var input = document.getElementById("messageText")
+    var input = document.getElementById("messageText");
     startTime = new Date();
     ws.send(input.value)
     input.value = ''
