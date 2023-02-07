84d25e10d9df38bfcd421cb66b733f5ec7b6300d
garcifer <garcifer@students.zhaw.ch>
2023-02-07 13:19:06 +0100

    rtt added

--- app/static/scripts.js
+++ app/static/scripts.js
@@ -1,26 +1,28 @@
 //var ws = new WebSocket("ws://localhost:8000/ws");
 var ws = new WebSocket("wss://art-intel.site/ws");
-var startTime = Date();
 
-document.addEventListener("DOMContentLoaded", function(){
+var startTime = int(Date());
 
-    ws.onmessage = function(event) {
-        var endTime = new Date();
-        var rtt = endTime - startTime;
-        var messages = document.getElementById('messages')
-        var message = document.createElement('li')
-        var text = event.data;
-        text += " (RTT: " + rtt + "ms)";
-        var content = document.createTextNode(text)
-        message.appendChild(content)
-        messages.appendChild(message)
-    };
-
-    function sendMessage(event) {
-        var input = document.getElementById("messageText")
-        startTime = new Date();
-        ws.send(input.value)
-        input.value = ''
-        event.preventDefault()
+ws.onmessage = function(event) {
+    var endTime = new Date();
+    var rtt = endTime - startTime;
+    var messages = document.getElementById('messages')
+    var message = document.createElement('li')
+    var text = event.data;
+    if (isNan(rtt)){
     }
-});
+    else{
+        text += " (RTT: " + rtt + "ms)";
+    }
+    var content = document.createTextNode(text)
+    message.appendChild(content)
+    messages.appendChild(message)
+};
+
+function sendMessage(event) {
+    var input = document.getElementById("messageText")
+    startTime = new Date();
+    ws.send(input.value)
+    input.value = ''
+    event.preventDefault()
+}
