af53760c8f74a9ec318e1460efe0d3dec77b9b42
garcifer <garcifer@students.zhaw.ch>
2023-02-07 13:14:10 +0100

    rtt added

--- app/static/scripts.js
+++ app/static/scripts.js
@@ -1,24 +1,26 @@
 //var ws = new WebSocket("ws://localhost:8000/ws");
 var ws = new WebSocket("wss://art-intel.site/ws");
-
 var startTime = Date();
 
-ws.onmessage = function(event) {
-    var endTime = new Date();
-    var rtt = endTime - startTime;
-    var messages = document.getElementById('messages')
-    var message = document.createElement('li')
-    var text = event.data;
-    text += " (RTT: " + rtt + "ms)";
-    var content = document.createTextNode(text)
-    message.appendChild(content)
-    messages.appendChild(message)
-};
+document.addEventListener("DOMContentLoaded", function(){
 
-function sendMessage(event) {
-   var input = document.getElementById("messageText")
-   startTime = new Date();
-   ws.send(input.value)
-   input.value = ''
-   event.preventDefault()
-}
+    ws.onmessage = function(event) {
+        var endTime = new Date();
+        var rtt = endTime - startTime;
+        var messages = document.getElementById('messages')
+        var message = document.createElement('li')
+        var text = event.data;
+        text += " (RTT: " + rtt + "ms)";
+        var content = document.createTextNode(text)
+        message.appendChild(content)
+        messages.appendChild(message)
+    };
+
+    function sendMessage(event) {
+        var input = document.getElementById("messageText")
+        startTime = new Date();
+        ws.send(input.value)
+        input.value = ''
+        event.preventDefault()
+    }
+});
