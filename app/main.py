5816aba58fed38ba5f7f1bc89b85f68e15ee2af2
garcifer <garcifer@students.zhaw.ch>
2023-02-07 12:34:38 +0100

    rtt added

--- app/main.py
+++ app/main.py
@@ -57,7 +57,7 @@ async def websocket_endpoint(websocket: WebSocket):
 if __name__ == '__main__':
     # start redirect process for port 80
     Popen(['python3', 'app/https_redirect.py'])
-    # run Ssl server
+    # run SSL server
     uvicorn.run(
         'main:app', port=443, host='0.0.0.0',
         reload=True,
--- app/static/scripts.js
+++ app/static/scripts.js
@@ -1,17 +1,21 @@
 //var ws = new WebSocket("ws://localhost:8000/ws");
 var ws = new WebSocket("wss://art-intel.site/ws");
 
+var startTime;
 
 ws.onmessage = function(event) {
-   var messages = document.getElementById('messages')
-   var message = document.createElement('li')
-   var content = document.createTextNode(event.data)
-   message.appendChild(content)
-   messages.appendChild(message)
+    var rtt = new Date() - startTime;
+    message += "RTT: " + rtt;
+    var messages = document.getElementById('messages')
+    var message = document.createElement('li')
+    var content = document.createTextNode(event.data)
+    message.appendChild(content)
+    messages.appendChild(message)
 };
 
 function sendMessage(event) {
    var input = document.getElementById("messageText")
+   startTime = new Date();
    ws.send(input.value)
    input.value = ''
    event.preventDefault()
