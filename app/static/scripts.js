e49f79f885eb1be814632fe3121571fbcaf726be
garcifer <garcifer@students.zhaw.ch>
2023-02-07 12:39:00 +0100

    rtt added

--- app/static/scripts.js
+++ app/static/scripts.js
@@ -5,10 +5,10 @@ var startTime;
 
 ws.onmessage = function(event) {
     var rtt = new Date() - startTime;
-    message = message + "RTT: " + rtt;
     var messages = document.getElementById('messages')
     var message = document.createElement('li')
     var content = document.createTextNode(event.data)
+    content += "RTT: " + rtt;
     message.appendChild(content)
     messages.appendChild(message)
 };
